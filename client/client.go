84b36824d90be43f33164fdbcb574a1db0f7257f
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2024-06-29 00:01:13 +0530

    chore: create client

---
+++ client/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func StartClient(serverAddr string, filePath string) {
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	sendFile(conn, file)
+}
