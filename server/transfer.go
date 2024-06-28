720a2315fd427ba2fba848f3d9b5fb6ce417ccaa
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2024-06-28 23:58:37 +0530

    chore: create transfer options

---
+++ server/transfer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type TransferRecord struct {
+	Filename  string
+	Timestamp time.Time
+	Success   bool
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	out, err := os.Create(filepath.Join("uploads", header.Filename))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	recordTransfer(header.Filename, err == nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("File uploaded successfully"))
+}
+
+func recordTransfer(filename string, success bool) {
+	historyMutex.Lock()
+	defer historyMutex.Unlock()
+
+	transferHistory = append(transferHistory, TransferRecord{
+		Filename:  filename,
+		Timestamp: time.Now(),
+		Success:   success,
+	})
+}
