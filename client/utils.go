d65849c2cab5c7456b3402b8c83735565976d8f8
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2024-06-29 00:03:48 +0530

    feat: add connect with server ping

---
+++ client/utils.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net"
+	"time"
+)
+
+func discoverServer() (string, error) {
+	conn, err := net.DialTimeout("udp", "255.255.255.255:8081", time.Second*2)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("DISCOVER"))
+	if err != nil {
+		return "", err
+	}
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf[:n]), nil
+}
